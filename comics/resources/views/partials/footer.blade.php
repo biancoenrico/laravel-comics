a3e3f17139f7f2cd4b0d9b1664004c265dd26678
enrico bianco <biancoenrico71@gmail.com>
2021-06-08 18:48:52 +0200

    footer

--- comics/resources/views/partials/footer.blade.php
+++ comics/resources/views/partials/footer.blade.php
@@ -1,5 +1,163 @@
 
 
 <footer>
-    <h1>ciao sono il footer</h1>
+    <div class="wrapper">
+        <div class="container">
+            <div class="flex">
+                <ul class="links">
+                    <li>
+                        <h3>
+                            DC COMICS
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                </ul>
+                <ul class="links">
+                    <li>
+                        <h3>
+                            DC
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">terms of use</a>
+                    </li>
+                    <li>
+                        <a href="#">privacy policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="#">ad choices</a>
+                    </li>
+                    <li>
+                        <a href="#">advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">subscirptions</a>
+                    </li>
+                    <li>
+                        <a href="#">talent workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">shop help</a>
+                    </li>
+                    <li>
+                        <a href="#">contact us</a>
+                    </li>
+                </ul>
+                <ul class="links">
+                    <li>
+                        <h3>
+                            SITES
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC power visa</a>
+                    </li>
+                </ul>
+                <ul class="links">
+                    <li>
+                        <h3>
+                            SHOP
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">shop DC collectibles</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="logo-bg">
+                    
+            </div>
+        </div>
+    </div>
+
+    <div class="sign-up">
+        <div class="container">
+            <div class="flex">
+
+                <div class="button">
+                    <a href="#">SIGN-UP NOW!</a>
+                </div>
+
+                <div class="follow">
+                    <ul>
+                        <li>
+                            <span>
+                                FOLLOW US
+                            </span>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-twitter-square"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-youtube-square"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-pinterest"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-map-marker-alt"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </footer>
